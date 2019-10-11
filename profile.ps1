b28273ad7d9102aa76f277a01436e551ea184618
Sam Willcocks <sam@wlcx.cc>
2019-10-11 16:03:04 +0100

    Add ctrl-w and ctrl-d emulation

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,6 @@
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadlineKeyHandler -Chord Ctrl+w -Function BackwardKillWord
+Set-PSReadlineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit
+
 New-Alias which get-command
 New-Alias unzip Expand-Archive
