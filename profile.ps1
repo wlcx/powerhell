f163fd882e4c35a6de03dbb48433e6377e16c9f1
Sam Willcocks <sam@wlcx.cc>
2019-10-21 14:43:47 +0100

    Add fzf, zlocation, EDITOR env var

--- profile.ps1
+++ profile.ps1
@@ -2,5 +2,11 @@ Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineKeyHandler -Chord Ctrl+w -Function BackwardKillWord
 Set-PSReadlineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit
 
+Remove-PSReadlineKeyHandler 'Ctrl+r'
+Import-Module PSFzf
+Import-Module ZLocation
+
+$Env:EDITOR = "codium"
+
 New-Alias which get-command
 New-Alias unzip Expand-Archive
